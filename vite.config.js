9c747269c99badd88be1336e2d8c64db8a9e0d46
Felipe <felipehohlenwerger@gmail.com>
2026-07-12 19:10:34 -0400

    adiciona suporte a PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,43 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],base:'/meu-ciclo-de-estudos/'
+  base:'/meu-ciclo-de-estudos/', // troque pelo nome EXATO do seu repositório GitHub
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'Ciclo de Estudos',
+        short_name: 'Ciclo',
+        description: 'Ciclo de estudos, sessões, revisões e estatísticas para concurso',
+        theme_color: '#1B1F24',
+        background_color: '#1B1F24',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '.',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
 })
